3693fc3246e89684bc37023ff00757230dd7d1b5
vsheva <valera.sheva@gmail.com>
2023-12-15 23:04:05 -0500

    Add validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   genders = ['male', 'female'];
-
   signupForm: FormGroup;
+
+  ngOnInit() {
+   this.signupForm =new FormGroup({
+    "username": new FormControl(null, Validators.required),
+     "email": new FormControl(null, [Validators.required, Validators.email]),
+     "gender": new FormControl('male'),
+   });
+  }
+
+  onSubmit() {
+  console.log(this.signupForm)
+  }
 }
